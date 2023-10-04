e5ddf370b1e4b220b921e0dc6a7dcda56edab1fb
Martin Aguilera <maaguilera040113@gmail.com>
2023-10-03 22:38:02 -0300

    borrar comentarios dentro de mis codigos carts.js y products.js

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -5,13 +5,11 @@ const cartsRouter = express.Router();
 
 const carts =[]
 
-// Ruta para crear un nuevo carrito (POST)
 cartsRouter.post('/', (req, res) => {
     try {
-      // Genera un ID único para el nuevo carrito (puedes usar una biblioteca como 'uuid')
+
       const newCartId = Date.now().toString();
   
-      // Crea un nuevo carrito con la estructura requerida
     const newCart = {
       id: newCartId,
       products: [],
@@ -19,16 +17,14 @@ cartsRouter.post('/', (req, res) => {
       
       let carts;
 
-      // Lee el archivo carts.json
       try {
       carts = JSON.parse(fs.readFileSync('src/data/carts.json', 'utf-8'));
     } catch (readError) {
         res.status(500).json({ error: 'Error interno del servidor al leer carts.json' });
         return;
       }
-      // Agrega el nuevo carrito al array de carrito
       carts.push(newCart);
-      // Escribe los datos actualizados en carts.json
+
       try {
       fs.writeFileSync('src/data/carts.json', JSON.stringify(carts, null, 2));
     } catch (writeError) {
@@ -41,9 +37,8 @@ cartsRouter.post('/', (req, res) => {
     }
   });
   
-    // Ruta para listar productos en un carrito
     cartsRouter.get('/:cid', (req, res) => {
-      // Implementa la lógica para obtener los productos en un carrito por su ID desde carrito.json
+
       const cartId = req.params.cid;
       const carts = JSON.parse(fs.readFileSync('src/data/carts.json', 'utf-8'));
       const cart = carts.find((c) => c.id === cartId);
@@ -54,21 +49,20 @@ cartsRouter.post('/', (req, res) => {
       }
     });
     
-    // Ruta para agregar un producto a un carrito
     cartsRouter.post('/:cid/product/:pid', (req, res) => {
-      // Implementa la lógica para agregar un producto a un carrito en carrito.json
+
       const cartId = req.params.cid;
       const productId = req.params.pid;
       const carts = JSON.parse(fs.readFileSync('src/data/carts.json', 'utf-8'));
       const cart = carts.find((c) => c.id === cartId);
       if (cart) {
-        // Verifica si el producto ya existe en el carrito
+
         const existingProduct = cart.products.find((p) => p.product === productId);
         if (existingProduct) {
-          // Si existe, incrementa la cantidad
+
           existingProduct.quantity++;
         } else {
-          // Si no existe, agrega el producto al carrito
+
           cart.products.push({ product: productId, quantity: 1 });
         }
         fs.writeFileSync('src/data/carts.json', JSON.stringify(carts, null, 2));
--- src/routes/products.js
+++ src/routes/products.js
@@ -5,10 +5,8 @@ const productsRouter  = express.Router();
 
 const product =[]
 
-// Ruta para agregar un nuevo producto (POST)
 productsRouter.post('/', (req, res) => {
 
-    // Genera un ID único
     let id=Date.now().toString();
 
     const {
@@ -22,17 +20,14 @@ productsRouter.post('/', (req, res) => {
         thumbnails,
     } = req.body;
 
-    // Valida que todos los campos obligatorios estén presentes
     if (
         !title || !description || !code || !price || !status || !stock || !category
         ){
         return res.status(400).json({ error: 'Campos obligatorios faltantes' });
         }
 
-    // Lee el archivo productos.json
     const products = JSON.parse(fs.readFileSync('src/data/products.json', 'utf-8'));
 
-    // Crea un nuevo producto con los campos requeridos
     const newProduct = {
         id: Date.now().toString(),
         title,
@@ -44,30 +39,25 @@ productsRouter.post('/', (req, res) => {
         category,
         thumbnails,
     };
-
-    // Agrega el nuevo producto al array de productos
     products.push(newProduct);
-    // Escribe los datos actualizados en productos.json
+
     fs.writeFileSync('src/data/products.json', JSON.stringify(products, null, 2));
     res.status(201).json(newProduct);
   });
 
   
-// Ruta para listar todos los productos
 productsRouter.get('/', (req, res) => {
   try {
-    // Lee el archivo productos.json y lo convierte en un objeto JavaScript
+
     const products = JSON.parse(fs.readFileSync('src/data/products.json', 'utf-8'));
     res.json(products);
   } catch (error) {
-    // Maneja cualquier error que pueda ocurrir durante la lectura del archivo
+
     res.status(500).json({ error: 'Error al leer products.json' });
   }
 });
 
-// Ruta para obtener un producto por ID
 productsRouter.get('/:pid', (req, res) => {
-  // Implementa la lógica para obtener un producto por su ID desde productos.json
   const productId = req.params.pid;
   const products = JSON.parse(fs.readFileSync('src/data/products.json', 'utf-8'));
   const product = products.find((p) => p.id === productId);
@@ -78,24 +68,19 @@ productsRouter.get('/:pid', (req, res) => {
   }
 });
 
-// Otras rutas según tus requerimientos
-// Ruta para actualizar un producto por ID (PUT)
+
 productsRouter.put('/:pid', (req, res) => {
     try {
       const productId = req.params.pid;
       const updatedProductData = req.body;
   
-      // Lee el archivo productos.json
       let products = JSON.parse(fs.readFileSync('src/data/products.json', 'utf-8'));
   
-      // Busca el producto por su ID
       const existingProduct = products.find((p) => p.id === productId);
   
       if (existingProduct) {
-        // Actualiza los campos del producto (excepto el ID)
         Object.assign(existingProduct, updatedProductData);
   
-        // Escribe los datos actualizados en productos.json
         fs.writeFileSync('src/data/products.json', JSON.stringify(products, null, 2));
   
         res.json(existingProduct);
@@ -107,25 +92,19 @@ productsRouter.put('/:pid', (req, res) => {
     }
   });
   
-  // Ruta para eliminar un producto por ID (DELETE)
   productsRouter.delete('/:pid', (req, res) => {
     try {
       const productId = req.params.pid;
-  
-      // Lee el archivo productos.json
+
       let products = JSON.parse(fs.readFileSync('src/data/products.json', 'utf-8'));
   
-      // Encuentra el índice del producto en el array
       const productIndex = products.findIndex((p) => p.id === productId);
   
       if (productIndex !== -1) {
-        // Elimina el producto del array
         products.splice(productIndex, 1);
-  
-        // Escribe los datos actualizados en productos.json
+        
         fs.writeFileSync('src/data/products.json', JSON.stringify(products, null, 2));
-  
-        res.status(204).send(); // Respuesta sin contenido (producto eliminado)
+        res.status(204).send();
       } else {
         res.status(404).json({ message: 'Producto no encontrado' });
       }
